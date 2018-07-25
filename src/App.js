f37dff607bd20939a0c2333f13dc546169c268da
develop-zw <wb_zhangwen@zhongan.io>
2018-07-25 18:19:09 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='ashdkkjashd'>
-        <div>asdas</div>
+      <div>asdas</div>
         qeweqweqeqwe
       </div>
     )
