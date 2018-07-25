d3447db4168974e78cfccd1db02a49cd84cae1f8
develop-zw <wb_zhangwen@zhongan.io>
2018-07-25 18:26:50 +0800

    fix123

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default class App extends Component {
       <div>asdas</div>
       <div>asdas</div>
       <div>asdas</div>
+      <div>asdas</div>
         qeweqweqeqwe
       </div>
     )
