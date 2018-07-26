38f829e443b3b8682e3d7a5e7f0a749a1603aa50
develop-zw <wb_zhangwen@zhongan.io>
2018-07-26 10:40:19 +0800

    all

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
-import React, { Component } from 'react';
-
-export default class App extends Component {
-  render() {
-    return (
-      <div className='ashdkkjashd'>
-      <div>asdas</div>
-      <div>asdas</div>
-      <div>asdas</div>
-      <div>asdas</div>
-      <div>asdas</div>
-      <div>asdas</div>
-      <div>asdas</div>
-        qeweqweqeqwe
-      </div>
-    )
-  }
+function sum(a, b) {
+  return a + b;
 }
+module.exports = sum;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import sum from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
